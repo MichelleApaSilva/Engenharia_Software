241e3fb81df07422ec00a5f260c4112c2886ff23
Michelle Silva <77756539+MichelleApaSilva@users.noreply.github.com>
2025-04-25 16:05:37 +0200

    Update ReservaControle.java

--- projeto_Michele/src/main/java/control/ReservaControle.java
+++ projeto_Michele/src/main/java/control/ReservaControle.java
@@ -1,69 +1,72 @@
 package control;
 
-import java.util.List;
+import model.Reserva;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import model.Reserva;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class ReservaControle {
-	private EntityManager em;
-	
-	public ReservaControle() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto");
-		em = emf.createEntityManager();
-	}
-	
-	public void inserir(Reserva objeto) {
-		try {
-            em.getTransaction().begin();
-            em.persist(objeto);
-            em.getTransaction().commit();
-         } catch (Exception ex) {
-            ex.printStackTrace();
-            em.getTransaction().rollback();
-         }
-	}
-	
-	public void alterar(Reserva objeto) {
-		 try {
-	            em.getTransaction().begin();
-	            em.merge(objeto);
-	            em.getTransaction().commit();
-	         } catch (Exception ex) {
-	            ex.printStackTrace();
-	            em.getTransaction().rollback();
-	         }
-	}
-	
-	public void excluir(Reserva objeto) {
-		try {
-            em.getTransaction().begin();
-            em.remove(objeto);
-            em.getTransaction().commit();
-         } catch (Exception ex) {
-            ex.printStackTrace();
-            em.getTransaction().rollback();
-         }
-	}
-	
-	public void excluirPorId(Integer id) {
-		try {
-            Reserva objeto = buscarPorId(id);
-            excluir(objeto);
-         } catch (Exception ex) {
-            ex.printStackTrace();
-         }
-	}
-	
-	public Reserva buscarPorId(Integer id) {
-		return em.find(Reserva.class, id);
-	}
-	
-	public List<Reserva> buscarTodos() {
-		String nomeClasse = Reserva.class.getName();
-		return em.createQuery("FROM " + nomeClasse).getResultList();
-	}
+    private EntityManager em;
 
+    public ReservaControle() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto");
+        em = emf.createEntityManager();
+    }
+
+    public void inserir(Reserva objeto) {
+        executarTransacao(em -> em.persist(objeto));
+    }
+
+    public void alterar(Reserva objeto) {
+        executarTransacao(em -> em.merge(objeto));
+    }
+
+    public void excluir(Reserva objeto) {
+        executarTransacao(em -> em.remove(objeto));
+    }
+
+    public void excluirPorId(Integer id) {
+        Reserva objeto = buscarPorId(id);
+        if (objeto != null) {
+            excluir(objeto);
+        }
+    }
+
+    public Reserva buscarPorId(Integer id) {
+        return em.find(Reserva.class, id);
+    }
+
+    public List<Reserva> buscarTodos() {
+        return criarConsulta("FROM Reserva").getResultList();
+    }
+
+    // Método auxiliar para criar consultas com parâmetros
+    private TypedQuery<Reserva> criarConsulta(String jpql, String parametro, String valor) {
+        TypedQuery<Reserva> query = em.createQuery(jpql, Reserva.class);
+        if (parametro != null && valor != null) {
+            query.setParameter(parametro, valor);
+        }
+        return query;
+    }
+
+    // Método auxiliar para executar transações
+    private void executarTransacao(TransacaoExecutor executor) {
+        try {
+            em.getTransaction().begin();
+            executor.executar(em);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+
+    // Interface funcional para o Executor de transações
+    @FunctionalInterface
+    private interface TransacaoExecutor {
+        void executar(EntityManager em);
+    }
 }
